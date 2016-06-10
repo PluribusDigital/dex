7870881645a9e130a3a047ca75f8c30dd295f8eb
Ben Morris <git@benmorris.net>
2016-06-09 20:20:01 -0400

    initial data service for getProvider

--- js/app.js
+++ js/app.js
@@ -42,56 +42,14 @@ app.controller("ProviderController", ['$scope', '$routeParams', '$location', 'Da
 function ($scope, $routeParams, $location, dataService) {
     $scope.pageTitle = "Provider Detail";
     $scope.usStates = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
-    $scope.provider = {
-        name: "Acme Health Care Providers, Inc.",
-        npi: "1234567",
-        address: "123 Main st., Fairbanks, AK 23323",
-        actions: [
-            {
-                actionName: "Terminate",
-                providerName: "Acme Health Care Providers, Inc.",
-                practiceAddress: "123 Main St, Suite 500, Fairbanks, AK",
-                date: "Today 3:30 p.m.",
-                source: "Alaska",
-                reason: "Documented Fraud",
-                statusName: "Review"
-            },
-            {
-                actionName: "Reinstate (Recip.)",
-                providerName: "Acme Health Care Providers, Inc.",
-                practiceAddress: "456 Aloha Lane, Kona, HI",
-                date: "5/5/2013",
-                source: "Hawaii",
-                reason: "Documented Fraud",
-                statusName: "Published"
-            },
-            {
-                actionName: "Reinstate",
-                providerName: "Acme Health Care Providers, Inc.",
-                practiceAddress: "123 Main St, Suite 500, Fairbanks, AK",
-                date: "5/1/2013",
-                source: "Alaska",
-                reason: "Documented Fraud",
-                statusName: "Published"
-            },
-            {
-                actionName: "Terminate (Recip.)",
-                providerName: "Acme Health Care Providers, Inc.",
-                practiceAddress: "456 Aloha Lane, Kona, HI",
-                date: "5/15/2012",
-                source: "Hawaii",
-                reason: "Documented Fraud",
-                statusName: "Published"
-            },
-            {
-                actionName: "Terminate",
-                providerName: "Acme Health Care Providers, Inc.",
-                practiceAddress: "123 Main St, Suite 500, Fairbanks, AK",
-                date: "5/1/2012",
-                source: "Alaska",
-                reason: "Documented Fraud",
-                statusName: "Published"
-            }
-        ]
-    };
+    $scope.provider = {};
+
+    // fetch details for the provider
+    $scope.getDetail = function () {
+        dataService.getProvider($routeParams.id, $scope.onProviderLoaded);
+    }
+    $scope.onProviderLoaded = function (data) {
+        $scope.provider = data;
+    }
+    $scope.getDetail();
 }]);
