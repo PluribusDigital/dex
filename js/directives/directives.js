2591247ff6bb10d831526eba5db26a98b45b9d0e
Jeff Farley <JeffreyMFarley@users.noreply.github.com>
2016-06-13 11:02:45 -0400

    Revert "Action Details"

--- js/directives/directives.js
+++
@@ -1,9 +0,0 @@
-app.directive('dexAddress', function() {
-  return {
-    restrict: 'A',
-    scope: {
-      address: '=dexAddress'
-    },
-    templateUrl: 'js/directives/address.html'
-  };
-});
