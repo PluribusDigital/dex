e4131e3ebcdd08cc90a4c3f77e0db6e460a466fe
Ben Morris <git@benmorris.net>
2016-06-20 13:43:25 -0400

    reports dashboard for CMS

--- js/controllers/reportsCtrl.js
+++ js/controllers/reportsCtrl.js
@@ -5,13 +5,32 @@ function ($scope, $routeParams, $location, dataService, $uibModal, SessionServic
   $scope.emailButtonClass = "usa-button-disabled";
   $scope.user = SessionService.getUser();
 
+  // Global Charts Config for Axes starting at 0
+  Chart.scaleService.updateScaleDefaults('linear', {
+      ticks: {
+          min: 0
+      }
+  })
+
+  // Chart Data & Options
   $scope.activeStatesChart = {
     data: [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
+      [65, 59, 55, 42, 30, 19, 15],
+      [75, 60, 62, 55, 25, 10, 15]
     ],
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    series: ['Series A', 'Series B']
+    labels: ['CA', 'TN', 'MI', 'OH', 'NY', 'AL', 'WA'],
+    series: ['Terminations', 'Reciprocal'],
+    options: {}
+  }
+  $scope.reviewChart = {
+    data: [2.5, 2.4, 2.1, 2.5, 3.0, 3.1, 3.0],
+    labels: ['5/20/16', '5/27/16', '6/3/16', '6/10/16', '6/17/16', '6/24/16', '7/1/16'],
+    options: {}
+  }
+  $scope.inventoryChart = {
+    data: [20, 21, 19, 17, 18, 17, 15],
+    labels: ['5/20/16', '5/27/16', '6/3/16', '6/10/16', '6/17/16', '6/24/16', '7/1/16'],
+    options: {}
   }
 
   $scope.drillStateActivity = function(stateAbbr){
